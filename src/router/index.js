17f82b7483eb83691b58c6031094e4b1eb7c7427
MichalKlyz-s <klyz.michal97@gmail.com>
2025-02-18 01:41:34 +0100

    Dodanie nazwy i ikony do karty strony

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import logo from '../assets/logo.png'
 
 Vue.use(VueRouter)
 
@@ -8,7 +9,8 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: { title: 'Organs', icon: logo }
   },
   {
     path: '/about',
@@ -25,5 +27,14 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
-
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || 'Organs'
+  const faviconPath = to.meta.icon
+  const link = document.querySelector("link[rel*='icon']") || document.createElement('link')
+  link.type = 'image/x-icon'
+  link.rel = 'icon'
+  link.href = faviconPath
+  document.head.appendChild(link)
+  next()
+})
 export default router
