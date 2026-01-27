2f81784743fdaed0a5ee3e8975dbf94bb73b2389
MichalKlyz-s <klyz.michal97@gmail.com>
2026-01-27 01:08:57 +0100

    Poprawienie metod POST oraz GET

--- src/modules/apiF.ts
+++ src/modules/apiF.ts
@@ -1,69 +1,57 @@
-// import {PostOptions } from '../types/api'
 import axios from "axios";
-// import { getCurrenHeaders } from './server_config.ts';
 
 export const dataOnly = (res) => (res ? res.data : res);
 
-// const defaultOptions: { timeout: number } = { timeout: 155000 };
 export const readError = (error) => {
   if (axios.isCancel(error)) {
     console.log("Request canceled", error);
     return Promise.reject(error);
   }
 };
+const defaultOptions = {
+  timeout: 300000,
+  withCredentials: false,
+  headers: {
+    Accept: "application/json",
+    'Content-Type': "application/json"
+  },
+};
 
 export const axiosPost = (
   relativeUrl,
   params,
-  //  options: PostOptions = {},
-  options = {},
+  cancelToken,
 ) => {
-  // const defaultOptions: PostOptions = {
-  const defaultOptions = {
-    timeout: 155000,
-    // headers: getCurrenHeaders(),
-    withCredentials: true,
-    isPureData: false,
-  };
-  const postOptions = {
+  const optionsPost = {
     ...defaultOptions,
-    ...options,
-    headers: {
-      // ...defaultOptions.headers,
-      // ...options.headers,
-    },
-  };
-
-  return axios
-    .post(
-      `http://172.20.10.11:8888/${relativeUrl}`,
-      postOptions.isPureData ? params : { ...params, user: "MK" },
-      postOptions,
+    cancelToken: cancelToken ? cancelToken.value : null,
+  }
+  return axios.post(
+    `http://localhost:8888/${relativeUrl}`,{
+       data: params.value
+  }, optionsPost
     )
-    .catch(readError);
+  .catch(readError);
 };
 
-export const axiosGet = (relativeUrl, params = {}) => {
-  let config = {
-    headers: {
-      Accept: "application/json",
-    },
+export const axiosGet = (relativeUrl, cancelToken) => {
+  const optionsGet = {
+    ...defaultOptions,
+    cancelToken: cancelToken ? cancelToken.value : null,
     params: {
-      data: params,
-    },
-  };
-  // http://192.168.52.195
-  // return axios.get(`http://172.20.10.10:8888/${relativeUrl}`, config)
+      data: ''
+    }
+  }
+  return axios.get(`http://localhost:8888/${relativeUrl}`, optionsGet).catch(readError);
+};
 
-  // return axios.get(`http://192.168.0.143:8888/${relativeUrl}`, config)
-
-  return axios.get(`http://172.20.10.11:8888/${relativeUrl}`, config);
-  // return Axios.get( `http://192.168.0.3:8888/${relativeUrl}`, {
-  //     ...{
-  //         timeout: 155000},
-  //     params:{
-  //         ...params,
-  //         user: 'MK',
-  //     },
-  // });
+export const axiosGetWParams = (relativeUrl, params = {}, cancelToken) => {
+  const optionsGet = {
+    ...defaultOptions,
+    cancelToken: cancelToken ? cancelToken.value : null,
+    params: {
+      data: params
+    }
+  }
+  return axios.get(`http://localhost:8888/${relativeUrl}`, optionsGet).catch(readError);
 };
